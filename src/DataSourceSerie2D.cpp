efe7010ec11a07c8a66b2fad05d791d0a6cca7bd
William Gathoye <william@gathoye.be>
2021-09-08 23:17:24 +0200

    Merge DataSource2D into DataSourceSerie2D - same files

--- src/DataSourceSerie2D.cpp
+++ src/DataSourceSerie2D.cpp
@@ -1,6 +1,15 @@
 #include "DataSourceSerie2D.hpp"
 
+// Constructors
+DataSourceSerie2D::DataSourceSerie2D():
+    m_list() {
+}
+
 // Getters
+const HeplList<Data2D>& DataSourceSerie2D::getList() {
+    return m_list;
+}
+
 HeplString DataSourceSerie2D::getSubject2() const {
     return m_subject2;
 }
@@ -10,10 +19,14 @@ char DataSourceSerie2D::getType2() const {
 }
 
 // Setters
-void DataSourceSerie2D::setSubject2(HeplString subject) {
+void DataSourceSerie2D::setList(const HeplList<Data2D>& list) {
+    m_list = list;
+}
+
+void DataSourceSerie2D::setSubject2(const HeplString& subject) {
     m_subject2 = subject;
 }
 
-void DataSourceSerie2D::setType2(char type) {
+void DataSourceSerie2D::setType2(const char& type) {
     m_type2 = type;
 }
--- src/DataSourceSerie2D.hpp
+++ src/DataSourceSerie2D.hpp
@@ -1,27 +1,31 @@
-#ifndef DATASOURCESERIE2D_HPP_INCLUDED
-#define DATASOURCESERIE2D_HPP_INCLUDED
+#ifndef DATASOURCESERIE2D
+#define DATASOURCESERIE2D
 
-#include "./lib/utils/src/HeplBaseException.hpp"
-#include "./lib/utils/src/HeplString.hpp"
+#include "./lib/utils/src/HeplList.hpp"
+#include "DataSource.hpp"
 #include "Data2D.hpp"
 
-class DataSourceSerie2D {
-
+class DataSourceSerie2D: public DataSource {
 
     private:
-
+        HeplList<Data2D> m_list;
         HeplString m_subject2;
         char m_type2;
 
-
     public:
+        // Constructors
+        DataSourceSerie2D();
+
         // Getters
+        const HeplList<Data2D>& getList();
         HeplString getSubject2() const;
         char getType2() const;
 
         // Setters
-        void setSubject2(HeplString subject);
-        void setType2(char type);
+        void setList(const HeplList<Data2D>& list);
+        void setSubject2(const HeplString& subject);
+        void setType2(const char& type);
 };
 
-#endif  // DATASOURCESERIE2D_HPP_INCLUDED
+
+#endif // DATASOURCESERIE2D
